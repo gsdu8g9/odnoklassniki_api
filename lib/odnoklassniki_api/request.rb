7168ebc99c821184f91b7f4cbd0f1eb3ad7a756b
Landria <landria@linux-4tpj.site>
2012-09-24 15:58:48 +0000

    Pagination requests

--- lib/odnoklassniki_api/request.rb
+++ lib/odnoklassniki_api/request.rb
@@ -2,17 +2,24 @@ require 'timeout'
 require 'faraday_middleware'
 require 'faraday'
 require 'odnoklassniki_api/errors'
+require 'odnoklassniki_api/connection'
 require 'digest/md5'
 require 'multi_json'
+require 'pry'
 
 module OdnoklassnikiAPI
   module Request
 
     def get api_method, options = {}
-      request(:get, api_method, options)
+      request(api_method, options)
+    end
+
+    def get_next_page response
+      get_next_page_data response
     end
 
     private
+
     def calculate_signature options
       signature = String.new('')
       secret_string = options[:access_token].to_s + secret_key.to_s
@@ -27,7 +34,7 @@ module OdnoklassnikiAPI
       Digest::MD5.hexdigest(signature)
     end
 
-    def request(method, api_method, options)
+    def request(api_method, options)
 
       options = options.merge method: api_method
       options = options.merge application_key: application_key
@@ -35,8 +42,18 @@ module OdnoklassnikiAPI
       signature = calculate_signature options
       options = options.merge sig: signature
 
+      response = get_data options
+
+      response.instance_eval <<-RESP
+        def options; #{options} end
+      RESP
+
+    response
+    end
+
+    def get_data options
       begin
-        response = connection.get{|request| request.params = options}
+        response = connection.get { |request| request.params = options }
 
         response_error = (response.status != 200) ? OdnoklassnikiAPI::Error::WrongStatusError : nil
 
@@ -56,5 +73,21 @@ module OdnoklassnikiAPI
 
       response
     end
+
+    def get_next_page_data response
+      result = nil
+      if response.respond_to?('hasMore') && (response.hasMore)
+        options = response.options
+        options.tap { |h| h.delete(:sig) }
+        options = options.merge pagingAnchor: response.pagingAnchor
+        signature =  calculate_signature options
+        options = options.merge sig: signature
+
+        result = get_data options
+      end
+
+      result
+    end
+
   end
 end
--- spec/lib/odnoklassniki_api_spec.rb
+++ spec/lib/odnoklassniki_api_spec.rb
@@ -8,6 +8,8 @@ describe OdnoklassnikiAPI do
   let(:access_token) { 'csipa.21-xxxxxxxxxxxxxxxxxxxxxxxxxxxx' }
   let(:url) { "http://api.odnoklassniki.ru/fb.do?access_token=csipa.21-xxxxxxxxxxxxxxxxxxxxxxxxxxxx&method=friends.get&application_key=application_key&sig=92cd12074a5d377e1e71fa2ff12f5f43" }
   let(:error_url) { "http://api.odnoklassniki.ru/fb.do?access_token=csipa.21-xxxxxxxxxxxxxxxxxxxxxxxxxxxx&method=frien.get&application_key=application_key&sig=5627bfd402b647ca4843468eb69691eb" }
+  let(:photo_url) {"http://api.odnoklassniki.ru/fb.do?access_token=csipa.21-xxxxxxxxxxxxxxxxxxxxxxxxxxxx&aid=1946947353&application_key=application_key&method=photos.getUserAlbumPhotos&sig=2cae57a1da7b212ca20f78428f20b357"}
+  let(:next_data_url){"http://api.odnoklassniki.ru/fb.do?access_token=csipa.21-xxxxxxxxxxxxxxxxxxxxxxxxxxxx&aid=1946947353&application_key=application_key&method=photos.getUserAlbumPhotos&pagingAnchor=LTMyMTY3NzExMzotNzk5MDk5NDU=&sig=0860d4a85ca615e55158ab8105e1a917"}
 
 
   describe "#test client initialization" do
@@ -31,6 +33,8 @@ describe OdnoklassnikiAPI do
     let(:normal_response) { '["CAAAAAAAAAAAAAAA","GGAAAAAAAAAAAAAA"]' }
     let(:app_error_response) { '{"error_code":101,"error_msg":"PARAM_API_KEY : Application not exist"}' }
     let(:no_method_response) { '3:METHOD : Method frien.get not found' }
+    let(:has_more_response) {'{"photos": [ {  "fid":"1675836441",      "caption":"XXXXXXXXXXx...",      "location":null,      "standard_url":"http://devserv.odnoklassniki.ru:8000/getImage?photoId=1675836441&photoType=0",      "preview_url":"http://odnoklassniki.ru?photoId=1675836441&photoType=2",      "mark_count":29,      "mark_bonus_count":4,      "mark_avg":"5+"     }, { "fid":"1946947353",      "caption":"Yyyyyyyyyyyyy",      "location":null,      "standard_url":"http://devserv.odnoklassniki.ru:8000/getImage?photoId=1946947353&photoType=0",      "preview_url":"http://odnoklassniki.ru?photoId=1946947353&photoType=2",      "mark_count":1,      "mark_bonus_count":0,      "mark_avg":"5.00"     }], "hasMore":true, "pagingAnchor":"LTMyMTY3NzExMzotNzk5MDk5NDU=", "totalCount" : 5}'}
+    let(:photo_response) {'{"photos":[{  "fid":"1675836441",      "caption":"XXXXXXXXXXx...",      "location":null,      "standard_url":"http://devserv.odnoklassniki.ru:8000/getImage?photoId=1675836441&photoType=0",      "preview_url":"http://odnoklassniki.ru?photoId=1675836441&photoType=2",      "mark_count":29,      "mark_bonus_count":4,      "mark_avg":"5+"     }, { "fid":"1946947353",      "caption":"Yyyyyyyyyyyyy",      "location":null,      "standard_url":"http://devserv.odnoklassniki.ru:8000/getImage?photoId=1946947353&photoType=0",      "preview_url":"http://odnoklassniki.ru?photoId=1946947353&photoType=2",      "mark_count":1,      "mark_bonus_count":0,      "mark_avg":"5.00"     }], "totalCount" : 5}'}
 
     it "should return users friends ids from get request" do
       stub_get_request(url, normal_response, 'application/json')
@@ -53,5 +57,35 @@ describe OdnoklassnikiAPI do
       result.should == OdnoklassnikiAPI::Error::ParsingError
     end
 
+    it "should return nil when calling next_page on not json response" do
+      stub_get_request(error_url, no_method_response, 'text/html')
+      result = @client.get 'frien.get'
+
+      @client.get_next_page(result).should == nil
+    end
+
+    it "should return nil when calling next_page on error response" do
+      stub_get_request(url, app_error_response, 'application/json')
+      result = @client.get 'friends.get'
+
+      @client.get_next_page(result).should == nil
+    end
+
+    it "should return nil when calling next_page on response with no hasMore" do
+      stub_get_request(url, normal_response, 'application/json')
+      result = @client.get 'friends.get'
+
+      @client.get_next_page(result).should == nil
+    end
+
+    it "should not return nil when calling next_page on response with hasMore" do
+      stub_get_request(photo_url, has_more_response, 'application/json')
+      stub_get_request(next_data_url, photo_response, 'application/json')
+
+      result = @client.get 'photos.getUserAlbumPhotos', {:aid => 1946947353}
+      @client.get_next_page(result).should_not == nil
+    end
+
   end
+
 end
